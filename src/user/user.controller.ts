fb3bdd4ea17c8affc1be8f9a0840cf0a70937654
VictorSouzaa0 <victor.r.souzaa05@gmail.com>
2026-07-05 12:49:30 -0300

    chore: GET, PUT method done

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,15 +1,17 @@
-import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Post,Put, Query, Param, ParseIntPipe } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UserModel } from 'generated/prisma/models';
 import { User } from 'generated/prisma/browser';
 import { listUserDto } from './dtos/list-user.dto';
+import { updateUserDto } from './dtos/update-user.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Get('all')
+  @Get('users')
+  @HttpCode(200)
   async getAllUsers(@Query() query: listUserDto): Promise<User[]>{
     return this.userService.users({
       where : {
@@ -21,11 +23,23 @@ export class UserController {
         }
     })
   };
-  
+
+  @Get(':id')
+  @HttpCode(200)
+  async getUserById(@Param('id') id: number): Promise<UserModel | null>{
+    return this.userService.user({id: Number(id)});
+  } 
+
   @Post('register')
+  @HttpCode(201)
   async create(@Body() incomingData: CreateUserDto): Promise<UserModel>{
     return await this.userService.createUser(incomingData);
   }
 
+  @Put(':id')
+  @HttpCode(200)
+  async updateUser(@Param('id', ParseIntPipe) id:number, @Body() updateUserDto: updateUserDto): Promise<User>{
+    return this.userService.updateUser(id,updateUserDto)
+  }
 
 }
