19ccfe22e0ad24f4f0640e70158c160efdc94a35
VictorSouzaa0 <victor.r.souzaa05@gmail.com>
2026-07-04 22:27:25 -0300

    chore:: add decorator query to initialize database query

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,31 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UserModel } from 'generated/prisma/models';
+import { User } from 'generated/prisma/browser';
+import { listUserDto } from './dtos/list-user.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Post()
+  @Get('all')
+  async getAllUsers(@Query() query: listUserDto): Promise<User[]>{
+    return this.userService.users({
+      where : {
+          id: query.id,
+          email: query.email,
+          name: query.name,
+          lastname: query.lastname,
+    
+        }
+    })
+  };
+  
+  @Post('register')
   async create(@Body() incomingData: CreateUserDto): Promise<UserModel>{
     return await this.userService.createUser(incomingData);
   }
+
+
 }
