5f5100e5916dc1eb5185fd63cd7a1843688c590f
VictorSouzaa0 <victor.r.souzaa05@gmail.com>
2026-07-04 22:25:40 -0300

    chore: get all user fixed

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,9 +2,33 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import * as bcrypt from 'bcrypt';
 import { Prisma, User } from 'generated/prisma/client';
+import { listUserDto } from './dtos/list-user.dto';
 @Injectable()
 export class UserService {
     constructor(private prisma: PrismaService,){}
+
+    async users(params: {
+        skip?: number;
+        take?: number;
+        cursor?: Prisma.UserWhereUniqueInput;
+        where?: Prisma.UserWhereInput;
+        orderBy?: Prisma.UserOrderByWithRelationInput;
+    }): Promise<any[]> {
+        const { skip, take, cursor, where, orderBy} = params ;
+        return this.prisma.user.findMany({
+            skip,
+            take,
+            cursor,
+            where,
+            orderBy,
+            select: {
+                id: true,
+                name: true,
+                lastname: true,
+                email: true,
+            }
+        });     
+    }
     async createUser(data: Prisma.UserCreateInput): Promise<User>{
         const saltOrRounds = 10;
         const password = data.password
