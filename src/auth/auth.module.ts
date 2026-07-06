e479c380af2169108fd822e526bac9e176ba6be1
VictorSouzaa0 <victor.r.souzaa05@gmail.com>
2026-07-06 15:57:20 -0300

    ci: auth module config

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,21 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { UserService } from 'src/user/user.service';
-import { JwtService } from '@nestjs/jwt';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
+  imports:[ PrismaModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async(configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions : {expiresIn:'1h'}
+      }),
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService, PrismaService, UserService, JwtService],
 })
