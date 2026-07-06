c3c1fe8c71528905819f7baac6481b0a4ccd9829
VictorSouzaa0 <victor.r.souzaa05@gmail.com>
2026-07-06 11:16:23 -0300

    chore: listdtobills done

---
+++ src/bill/dtos/list-bill.dto.ts
@@ -0,0 +1,19 @@
+import { IsOptional } from "class-validator";
+
+export class listBillDto{
+
+    @IsOptional()
+    readonly id?: number
+
+    @IsOptional()
+    readonly bills_name?: string
+    
+    @IsOptional()
+    readonly bills_balance?: number
+
+    @IsOptional()
+    readonly when_used?: number
+
+    @IsOptional()
+    readonly accountId?: number
+}
