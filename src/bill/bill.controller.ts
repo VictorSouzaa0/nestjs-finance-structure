46349c549134b35efefbaf5bfafab12df19ba6b3
VictorSouzaa0 <victor.r.souzaa05@gmail.com>
2026-07-06 11:17:28 -0300

    chore: get all bills working

--- src/bill/bill.controller.ts
+++ src/bill/bill.controller.ts
@@ -1,7 +1,10 @@
-import { Body, Controller, Delete, HttpCode, ParseIntPipe, Post, Param } from '@nestjs/common';
+import { Body, Controller, Delete, HttpCode, ParseIntPipe, Post, Param, Get, Query } from '@nestjs/common';
 import { BillService } from './bill.service';
 import { createBillDto } from './dtos/create-bill.dto';
 import { BillModel } from 'generated/prisma/models';
+import { listBillDto } from './dtos/list-bill.dto';
+import { retry } from 'rxjs';
+import { Bill } from 'generated/prisma/client';
 
 @Controller('bill')
 export class BillController {
@@ -11,6 +14,19 @@ export class BillController {
   @HttpCode(201)
   async createBill(@Body() incomingData: createBillDto): Promise <BillModel>{
     return await this.billService.createBill(incomingData)
+  }
+
+  @Get()
+  @HttpCode(200)
+  async getAllBills(@Query() query: listBillDto): Promise <Bill[]>{
+    return await this.billService.allBills({
+      where:{
+        id: query.id,
+        bills_name: query.bills_name,
+        bills_balance: query.bills_balance,
+        accountId: query.accountId
+      }
+    })
   }
 
   @Delete(':id')
